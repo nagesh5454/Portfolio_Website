e2bbe14e40da2c0d3ca02856eb8d64d6313cf72b
NageshMane <mane46740@gmail.com>
2024-09-16 12:44:02 +0530

    code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
             </Link>
             <a
               className="bg-yellow-500 hover:opacity-80 px-4 py-2 rounded-sm text-white font-semibold flex items-center"
-              href="/resume.pdf"
+              href="Nageshmane_Resume_Latest.pdf"
               download="Nageshmane_Resume_Latest.pdf"
             >
               Resume{" "}
